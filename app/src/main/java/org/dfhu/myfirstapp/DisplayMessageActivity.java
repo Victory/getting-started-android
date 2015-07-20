15af9304b51323e03caff2bef1f65c2293bb0d4c
Victory <git@dfhu.org>
2015-07-19 20:30:42 -0400

    remove unneeded method

--- app/src/main/java/org/dfhu/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/org/dfhu/myfirstapp/DisplayMessageActivity.java
@@ -14,13 +14,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_display_message, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long

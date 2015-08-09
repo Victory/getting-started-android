87cd23df0514fa2db96645014134c741a0adb472
Victory <git@dfhu.org>
2015-08-09 18:14:39 -0400

    Switch back ActionBarActivity so we can create a whole other activity in AppCompat

--- app/src/main/java/org/dfhu/myfirstapp/NowWithDrawerActivity.java
+++ app/src/main/java/org/dfhu/myfirstapp/NowWithDrawerActivity.java
@@ -2,11 +2,11 @@ package org.dfhu.myfirstapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +15,7 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 
 
-public class NowWithDrawerActivity extends AppCompatActivity
+public class NowWithDrawerActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**

ed04ad4a610ed5b541fb4a3fc581cf37f81d9440
Victory <git@dfhu.org>
2015-08-09 11:51:15 -0400

    Switch Drawer to AppCompat

--- app/src/main/java/org/dfhu/myfirstapp/NowWithDrawerActivity.java
+++ app/src/main/java/org/dfhu/myfirstapp/NowWithDrawerActivity.java
@@ -2,24 +2,20 @@ package org.dfhu.myfirstapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
-public class NowWithDrawerActivity extends ActionBarActivity
+
+public class NowWithDrawerActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
